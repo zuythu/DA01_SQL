a68e4756aea0a4638aece895be8aed4c74622ea5
zuythu <155988057+zuythu@users.noreply.github.com>
2024-01-17 08:51:01 +0700

    Update and rename buoi09.sql to day09.sql

--- day09.sql
+++ day09.sql
@@ -19,6 +19,13 @@ CASE
 END AS triangle
 FROM Triangle;
 
+--ex03
+SELECT 
+ROUND(100.0 * COUNT(case_id)/ (SELECT COUNT(*) FROM callers),1) AS call_percentage
+FROM callers
+WHERE call_category IS NULL 
+OR call_category = 'n/a';
+
 --ex04
 # Write your MySQL query statement below
 SELECT name FROM Customer
@@ -26,3 +33,19 @@ WHERE NOT referee_id = 2
 OR referee_id IS NULL;
 
 --ex05
+SELECT
+survived,
+SUM(CASE 
+ WHEN pclass=1 THEN 1
+ ELSE 0
+END) AS first_class,
+SUM(CASE 
+ WHEN pclass=2 THEN 1
+ ELSE 0
+END) AS second_class,
+SUM(CASE
+ WHEN pclass=3 THEN 1
+ ELSE 0
+END) AS third_class
+FROM titanic
+GROUP BY survived;
