326fae6a448f395bca934c171575a33e07b27965
zuythu <155988057+zuythu@users.noreply.github.com>
2024-01-25 15:27:09 +0700

    Update and rename buoi12.sql to day12.sql

--- day12.sql
+++ day12.sql
@@ -10,3 +10,12 @@ GROUP BY year, product_id, spend
 ORDER BY product_id, year;
 
 --ex02
+WITH cte AS
+(SELECT *,
+ROW_NUMBER () OVER(PARTITION BY card_name ORDER BY issued_amount, issue_month, issue_year) AS ranking
+FROM monthly_cards_issued)
+SELECT card_name, issued_amount
+FROM cte
+WHERE ranking = 1
+ORDER BY issued_amount DESC;
+
