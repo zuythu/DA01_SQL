9b9f712f3ab7f35e45d7d25359b44c3075b6af6e
zuythu <155988057+zuythu@users.noreply.github.com>
2024-02-03 19:11:54 +0700

    Update day21.sql

--- day21.sql
+++ day21.sql
@@ -1,1 +1,14 @@
-
+--ex01
+select year||'-'||month as year_month,
+total_user,
+total_order
+from (
+select
+sum(distinct user_id) as total_user,
+sum(distinct order_id) as total_order,
+extract(month from created_at) as month,
+extract(year from created_at) as year
+from bigquery-public-data.thelook_ecommerce.order_items
+group by year, month
+order by year, month)
+limit 40
