e61816253d337b699dbbe824c6ab467972ad39fc
zuythu <155988057+zuythu@users.noreply.github.com>
2024-02-03 19:29:25 +0700

    Update day21.sql

--- day21.sql
+++ day21.sql
@@ -12,3 +12,13 @@ from bigquery-public-data.thelook_ecommerce.order_items
 group by year, month
 order by year, month)
 limit 40
+--> tổng đơn hàng và khách hàng theo từng tháng tăng từ 1/2019 đến 4/2022
+--ex02
+SELECT 
+FORMAT_TIMESTAMP('%Y-%m', created_at) AS year_month,
+count(distinct user_id) as distinct_users,
+avg(sale_price) as average_order_value
+FROM bigquery-public-data.thelook_ecommerce.order_items
+where FORMAT_TIMESTAMP('%Y-%m', created_at) between '2019-01' and '2022-04'
+group by year_month
+order by year_month
