c0de450a789decb658e580078a606af101e0d7a3
zuythu <155988057+zuythu@users.noreply.github.com>
2024-02-03 23:21:58 +0700

    Update day21.sql

--- day21.sql
+++ day21.sql
@@ -22,3 +22,29 @@ FROM bigquery-public-data.thelook_ecommerce.order_items
 where FORMAT_TIMESTAMP('%Y-%m', created_at) between '2019-01' and '2022-04'
 group by year_month
 order by year_month
+
+--ex03
+with cte as (select last_name,first_name, age, gender,
+FORMAT_TIMESTAMP('%Y-%m', created_at) AS year_month
+from bigquery-public-data.thelook_ecommerce.users
+where FORMAT_TIMESTAMP('%Y-%m', created_at) between '2019-01' and '2022-04'),
+cte2 as (
+SELECT
+  last_name,
+  first_name,
+  age,
+  gender,
+  CASE
+    WHEN age = (SELECT MAX(age) FROM cte WHERE gender = 'M') THEN 'oldest'
+    WHEN age = (SELECT MAX(age) FROM cte WHERE gender = 'F') THEN 'oldest'
+    WHEN age = (SELECT MIN(age) FROM cte WHERE gender = 'M') THEN 'youngest'
+    WHEN age = (SELECT MIN(age) FROM cte WHERE gender = 'F') THEN 'youngest'
+  END AS tag
+FROM
+  cte)
+
+SELECT tag, COUNT(*) as number 
+FROM cte2
+GROUP BY tag;
+
+--ex04
